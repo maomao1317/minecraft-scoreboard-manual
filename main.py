39140978bebe1a80c56a9ad18a4ddb18b4ab7ca7
orwtmc <rsow001@gmail.com>
2019-12-31 19:32:38 +0800

    Add function: list all scoreboard

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+# import system function
 import os
+# import user dump
 import json
+
+# list all scoreboard
+def listall():
+    print("This is the command: ")
+    with open("commands/listallcommand.cmds", 'r') as f_obj:
+        cmd = f_obj.read()
+        print(cmd)
+
+# user run
+print("Adjust you can execute commands: ")
+print("1. List all exist scoreboards")
+
+options = input("Option Number: ")
+
+# judge options
+if options == "1":
+    listall()
